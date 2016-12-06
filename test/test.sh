39633d3f487fa2a4918563cb0482f362888ae3a0
lijiang2014 <lijianggs@126.com>
2016-12-06 15:59:44 +0800

    0.0.2 fixs

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,6 @@
-src_path=/HOME/nscc-gz_jiangli/virtualenv/ht_celery
-export PATH=$src_path:$PATH
+#src_path=/HOME/nscc-gz_jiangli/virtualenv/ht_celery
+export THHT_PATH=/WORK/app/share/py34env-rh6.5
+#export PATH=$THHT_PATH:$PATH
 export THHT_EXEC="/HOME/nscc-gz_jiangli/virtualenv/ht_celery/simtask.py"
 export THHT_INPUT="input.htc"
-yhbatch -N 2 $src_path/main.sh settings.htc
+yhbatch -N 2 $THHT_PATH/thht/main.sh settings.htc
