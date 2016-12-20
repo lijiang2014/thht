c5c6e2b5b3fb55ba7ee86c950a7cf8d5bc53af6a
lijiang2014 <lijianggs@126.com>
2016-12-20 10:44:33 +0800

    update test

--- test/clean.sh
+++ test/clean.sh
@@ -2,4 +2,4 @@
 #   killall -9 celery
 #   killall -9 python3
 #   killall -9 redis-server
-rm -rf out.* log.* *.conf *.rdb *.out output/out.* output/err.* *.pid *.json
+rm -rf out.* log.* *.conf *.rdb *.out output/out.* output/err.* *.pid *.json task_state 
